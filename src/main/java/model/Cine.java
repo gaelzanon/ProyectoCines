56ab3690a3c10741fd85c1f76c970b3cefc1f498
Gael Zanón <al288187@uji.es>
2022-03-03 15:13:14 +0100

    Clase cine

---
+++ src/main/java/model/Cine.java
@@ -0,0 +1,34 @@
+package model;
+
+//@Entity
+public class Cine {
+    private String nombre;
+    private String ubicacion;
+
+    public Cine() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cine{" +
+                "nombre='" + nombre + '\'' +
+                ", ubicacion='" + ubicacion + '\'' +
+                '}';
+    }
+}
