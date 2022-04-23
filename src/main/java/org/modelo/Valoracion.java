1a190cd285cb4b13fd1acefbb892e9bfa2d760c1
MarcosJovani <marcos.jovani98@gmail.com>
2022-04-24 00:01:39 +0200

    valoraciones

---
+++ src/main/java/org/modelo/Valoracion.java
@@ -0,0 +1,18 @@
+package org.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Valoracion {
+    @Id
+    private String id;
+    private String usuario;
+    private String pelicula;
+    private int puntuacion;
+    private String Comentario;
+
+
+    public Valoracion() {
+    }
+}
